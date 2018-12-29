06dc076341bd0f0ea56623c789b5bc05488391ae
F. Javier R. Donado <fjrd84@gmail.com>
2018-12-29 19:48:16 +0100

    Add a TTS example

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,6 +4,16 @@ import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
 
+const saySomething = (text, lang = 'en-US') => {
+  const message = new SpeechSynthesisUtterance();
+  const voice = window.speechSynthesis
+    .getVoices()
+    .filter(it => it.lang === lang)[0];
+  message.text = text;
+  message.voice = voice;
+  speechSynthesis.speak(message);
+};
+
 type Props = {};
 
 export default class Home extends Component<Props> {
@@ -14,6 +24,13 @@ export default class Home extends Component<Props> {
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
+        <button
+          onClick={() => saySomething('This is a wonderful test!!')}
+          type="button"
+        >
+          {' '}
+          Now say something nice!
+        </button>
       </div>
     );
   }
