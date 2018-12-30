1c7b86a59e03e9714f138c6c63ef00eab89ca773
F. Javier R. Donado <fjrd84@gmail.com>
2018-12-30 17:35:39 +0100

    Use a specific callback for the token

--- app/config.js
+++ app/config.js
@@ -7,6 +7,8 @@ export default {
     clientId: process.env.SPOTIFY_CLIENT_ID || 'edit-your-dot-env-file',
     spotifyClientSecret:
       process.env.SPOTIFY_CLIENT_SECRET || 'edit-your-dot-env-file',
-    redirectUri: process.env.REDIRECT_URI || 'http://localhost'
+    redirectUri: process.env.REDIRECT_URI || 'http://localhost/tokencallback',
+    scopes:
+      'user-read-playback-state user-modify-playback-state playlist-read-collaborative playlist-read-private playlist-modify-public playlist-modify-private user-library-modify'
   }
 };
