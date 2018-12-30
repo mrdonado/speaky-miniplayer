07da53e820ffe650bd305b1ae872e24aa9a8a877
F. Javier R. Donado <fjrd84@gmail.com>
2018-12-30 17:41:18 +0100

    Add example: get Spotify API token

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,8 +1,10 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { remote } from 'electron';
 import routes from '../constants/routes';
 import styles from './Home.css';
+import config from '../config';
 
 const saySomething = (text, lang = 'en-US') => {
   const message = new SpeechSynthesisUtterance();
@@ -12,6 +14,41 @@ const saySomething = (text, lang = 'en-US') => {
   message.text = text;
   message.voice = voice;
   speechSynthesis.speak(message);
+};
+
+const getToken = () => {
+  const authWindow = new remote.BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    'node-integration': false,
+    'web-security': false
+  });
+
+  const authUrl = `https://accounts.spotify.com/en/authorize?client_id=${
+    config.spotify.clientId
+  }&response_type=code&redirect_uri=${encodeURIComponent(
+    config.spotify.redirectUri
+  )}&scope=${encodeURI(config.spotify.scopes)}`;
+
+  authWindow.loadURL(authUrl);
+
+  authWindow.show();
+
+  authWindow.webContents.session.webRequest.onBeforeRequest(
+    {
+      urls: [`${config.spotify.redirectUri}*`]
+    },
+    (details, callback) => {
+      const token = details.url.replace(
+        `${config.spotify.redirectUri}?code=`,
+        ''
+      );
+      console.log(token);
+      callback({ cancel: false });
+      authWindow.close();
+    }
+  );
 };
 
 type Props = {};
@@ -24,12 +61,20 @@ export default class Home extends Component<Props> {
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
+
         <button
-          onClick={() => saySomething('This is a wonderful test!!')}
+          onClick={() =>
+            saySomething(
+              'You just heard whatever by whoever, from the album whatever'
+            )
+          }
           type="button"
         >
-          {' '}
-          Now say something nice!
+          Inform me now!
+        </button>
+
+        <button onClick={() => getToken()} type="button">
+          Get the token!!
         </button>
       </div>
     );
